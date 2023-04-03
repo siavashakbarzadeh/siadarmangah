32dc71cd90070e18d334f79d9b6c3b04b0a03a08
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-04-03 14:13:09 +0200

    Update member-payment.blade.php

--- resources/views/pdf/member-payment.blade.php
+++ resources/views/pdf/member-payment.blade.php
@@ -212,7 +212,7 @@
         </div>
 
     </div>
-    <div style="width: 100%;height: 40px;border: solid 1px red;margin-right: 10px;">
+    <div style="width: 90%;height: 40px;border: solid 1px red;margin-right: 10px;">
         kdlkjkljd
     </div>
     <div style="position: fixed;top: 200px;right: -85px;width: 230px;height: 20px;transform: rotate(-90deg);font-size: 7px;">

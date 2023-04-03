8ba71741c76e05ca2ea2b1927cb69d86959eb663
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-04-03 16:27:32 +0200

    Update member-payment.blade.php

--- resources/views/pdf/member-payment.blade.php
+++ resources/views/pdf/member-payment.blade.php
@@ -197,7 +197,7 @@
                 </div>
             </div>
 
-            <div style="margin:-15px 0px 0px; ;padding: 5px 4px 0px;clear: both;font-weight: bold;height:20px;width: 100%;border: solid 1px red;"  >
+            <div style="margin:-30px 0px 0px; ;padding: 5px 4px 0px;clear: both;font-weight: bold;height:20px;width: 100%;border: solid 1px red;"  >
                 <span style="width: 40%;font-size: 8px;text-transform: uppercase;padding-left: 10px;list-style-type: none;font-weight: bold;">
 
                     <li>bollo dell'ufficio postale</li>

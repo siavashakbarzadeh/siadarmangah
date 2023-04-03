40eb60c2559b65966ed44a92fc8d5f60d23ea397
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-04-03 14:58:49 +0200

    Update member-payment.blade.php

--- resources/views/pdf/member-payment.blade.php
+++ resources/views/pdf/member-payment.blade.php
@@ -215,13 +215,14 @@
         </div>
 
     </div>
-    <div style="padding: 0px 11px;">
-        <div  style="width:100%;height:60px;margin:0px auto ;border: solid 1px red;">
-            kdlkjkljd
-        </div>
-    </div>
+
     <div style="position: fixed;top: 200px;right: -85px;width: 230px;height: 20px;transform: rotate(-90deg);font-size: 7px;">
         Mod CH 8 ter-Mod12409A-ed1/10-EP1902/EP1903-St.[3]
+    </div>
+</div>
+<div style="padding: 0px 11px;">
+    <div  style="width:100%;height:60px;margin:0px auto ;border: solid 1px red;">
+        kdlkjkljd
     </div>
 </div>
 </body>
